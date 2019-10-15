ea1d8a7d4421b81ca5613b065ac4b0a0986d7770
emanuelecoppo <emanuelecoppo@gmail.com>
2019-10-15 17:14:59 +0200

    Aggiornato

--- sketch.js
+++ sketch.js
@@ -29,25 +29,41 @@ function setup() {
   slider2 = createSlider(0, 1, 0, 0); //delay
   slider3 = createSlider(.7, 1.3, 1, 0); //tuning
   slider4 = createSlider(0, 1, .5, 0); //volume
+
+  fft = new p5.FFT();
 }
 
 function draw() {
   background("white");
 
+  push();
+  let waveform = fft.waveform();
+  noFill();
+  stroke(0);
+  strokeWeight(2);
+  beginShape();
+  for (var i = 0; i < waveform.length; i+=5){
+    let x = map(i, 0, waveform.length, -width*0.1, width*1.1);
+    let y = map(waveform[i], -2, 2, 0, 650);
+    curveVertex(x, y);
+  }
+  endShape();
+  pop();
+
   imageMode(CENTER);
   image(gtr, width/3.5, 0, 1200, 1200);
 
-  slider1.position(width*.6, 150);
-  slider2.position(width*.6, 250);
-  slider3.position(width*.6, 350);
-  slider4.position(width*.6, 450);
+  slider1.position(width*.6, 150-50);
+  slider2.position(width*.6, 250-50);
+  slider3.position(width*.6, 350+80);
+  slider4.position(width*.6, 450+80);
 
   textSize(22);
   textStyle(BOLD);
-  text("Distortion", width*.6, 150-10);
-  text("Echo", width*.6, 250-10);
-  text("Tuning", width*.6, 350-10);
-  text("Volume", width*.6, 450-10);
+  text("Distortion", width*.6, 150-10-50);
+  text("Echo", width*.6, 250-10-50);
+  text("Tuning", width*.6, 350-10+80);
+  text("Volume", width*.6, 450-10+80);
 
   let val1 = slider1.value();
   let val2 = slider2.value();
